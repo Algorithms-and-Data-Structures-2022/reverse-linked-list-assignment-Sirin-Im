3dfc45f055b7c6b7022b496708c416d46e9008c7
Sirin <sirin3002imamov@gmail.com>
2022-04-14 23:28:01 +0300

    finished task

--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -12,7 +12,22 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseIterative() {
-    // Напишите здесь свой код ...
+    if (size_ <=  1){
+      return;
+    }
+
+    Node* current = front_;
+    Node* prev  = nullptr;
+    while (current != back_) {
+      Node* node = current->next;
+      current->next = prev;
+      prev = current;
+      current = node;
+    }
+    current->next = prev;
+    Node* node = back_;
+    back_ = front_;
+    front_ = node;
   }
 
   /**
@@ -23,12 +38,19 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseRecursive() {
-    // Напишите здесь свой код ...
+    reverse_recursive_helper(front_, nullptr);
+    Node* node = front_;
+    front_ = back_;
+    back_ = node;
   }
 
   // вспомогательный метод для реализации рекурсии
   void LinkedList::reverse_recursive_helper(Node*& curr, Node* prev) {
-    // Напишите здесь свой код ...
+    if (curr == nullptr) {
+      return;
+    }
+    reverse_recursive_helper(curr->next, curr);
+    curr->next = prev;
   }
 
 }  // namespace assignment
